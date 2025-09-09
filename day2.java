e666eb1b5dbecee438d8ab364938b5a116129ca2
nuralim25-gif <alimnuralim0411@gmail.com>
2025-09-09 14:18:41 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,17 @@
+public class day2 {
+    
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        
+        //batas print
+        
+        System.out.println("Halo, teman-teman");
+        System.out.println("Nama saya Nur Alim");
+        
+        //batas println
+        
+        String nama = "Nur Alim";
+        int umur = 17;
+        System.out.printf("Nama: %s, Umur: %d tahun%n", nama, umur);
+    } 
+}
