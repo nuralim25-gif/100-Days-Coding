31eba7139a3ca4d3ce32e2b3a816e5a4d2b0c962
nuralim25-gif <alimnuralim0411@gmail.com>
2025-09-10 12:30:11 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,12 @@
+public class day3 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //Ini adalah komentar untuk satu baris yang ditandai dengan dua slash di depan komentar.   
+        /*Ini adalah komentar untuk beberapa baris.
+          Cocok digunakan untuk memberikan penjelasan pada sebuah codingan atau untuk menonaktifkan sebuah codingan.
+          Komentar ini ditandai dengan slash asterisk di awal dan asterisk slash di akhir komentar.
+         */
+    }
+    
+}
