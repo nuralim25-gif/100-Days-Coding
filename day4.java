db86150c47b80feab1d376d6c017ea4eb1a9d6a7
nuralim25-gif <alimnuralim0411@gmail.com>
2025-09-11 09:22:32 +0800

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,25 @@
+package PenggunaanEscapeSequence;
+
+public class day4 {
+    
+    public static void main(String[] args) {
+        // Penggunaan \n = newline (baris baru/pindah baris)
+        System.out.println("Hello, teman-teman!\nBerikut biodata saya:");
+        
+        //Penggunaan \t = tab (spasi lebar)
+        System.out.println("Nama:\tNur Alim");
+        System.out.println("Umur:\t17 tahun");
+        System.out.println("Alamat:\tTeppo");
+        
+        //Penggunaan \" = Menampilkan tanda kutip ganda
+        System.out.println("Saya akrab dipanggil \"Alim\"");
+        
+        //Penggunaan \\ = Menampikan backslash (\)
+        System.out.println("Lokasi file: Penyimpanan di Perangkat\\100 Days Of Coding");
+        
+        //Penggunaan \' = Menampilkan tanda kutip tunggal
+        System.out.println("Huruf awal nama saya: \'N\'");
+        
+    }
+    
+}
