13972732d5ba2d23d1de056e937ad412d7f411ca
nuralim25-gif <alimnuralim0411@gmail.com>
2025-09-12 13:05:23 +0800

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,21 @@
+package TipeDataNumerikBilanganBulat;
+
+public class day5 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        byte a = 127;
+        short b = 32767;
+        int c = 2000000;
+        long d = 10000000000000L;
+        
+        System.out.println("byte:" + a);
+        System.out.println("short:" + b);
+        System.out.println("int:" + c);
+        System.out.println("long:" + d);
+        
+    }
+    
+    
+    
+}
