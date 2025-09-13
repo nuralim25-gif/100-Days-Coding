1351251eb69a43b50b13f510c594c549aea2a0d2
nuralim25-gif <alimnuralim0411@gmail.com>
2025-09-13 15:44:16 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,25 @@
+public class day6 {
+    
+    public static void main(String[] args) {
+        
+        //Tipe data float (diakhiri f
+        float f = 5.57f;
+        
+        //Tipe data double (default untuk angka desimal)   
+        double d = 5.123456789;
+        
+        //Cetak nilai float dan double
+        System.out.println("Nilai Float\t\t: " + f);
+        System.out.println("Nilai Double\t\t: " + d);
+        
+        //Operasi matematika
+        float hasilfloat = f * 5;
+        double hasildouble = d / 2;
+            
+        //Cetak hasil operasi matematika
+        System.out.println("Hasil Perkalian Float\t: " + hasilfloat);
+        System.out.println("Hasil Pembagian Double\t: " + hasildouble);
+            
+    }
+    
+}
