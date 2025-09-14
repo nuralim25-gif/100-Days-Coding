d121ecd5e0d133c548aaf21a5450df2444103e6d
nuralim25-gif <alimnuralim0411@gmail.com>
2025-09-14 19:32:16 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,28 @@
+public class day7 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        char huruf = 'A';
+        char angka = '1';
+        char karakter = '@';
+        
+        System.out.println("Huruf:" + huruf);
+        System.out.println("Angka:" + angka);
+        System.out.println("Karakter:" + karakter);   
+        
+        boolean siap = true;
+        boolean tidak = false;
+        
+        System.out.println("Siapkah kamu untuk pertemuan ini?" + siap);    
+        System.out.println("Kamu tidak siap?" + tidak);
+        
+        if (siap) {
+            System.out.println("Nice");
+        } else {
+            System.out.println("Why?");
+        }
+        
+    }
+    
+}
