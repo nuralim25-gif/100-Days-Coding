1f9d95e62fcf3d8e03fdfda459de979e39dd3a67
nuralim25-gif <alimnuralim0411@gmail.com>
2025-09-23 03:40:16 +0800

    Create day16.java

---
+++ day16.java
@@ -0,0 +1,22 @@
+public class day16 {
+    
+    public static void main(String[] args) {
+        
+        //Angka yang akan dioperasikan
+        int angka1 = 10;
+        int angka2 = 5;
+        
+        //Operasi perkalian
+        int hasilkali = angka1 * angka2;
+        
+        //Pengurangan
+        int hasilbagi = angka1 / angka2;
+        
+        //Menampilkan hasil
+        System.out.println("Angka pertama: " + angka1);
+        System.out.println("Angka kedua  : " + angka2);     
+        System.out.println("\nHasil Penjumlahan: " + hasilkali);
+        System.out.println("Hasil Pengurangan: " + hasilbagi);
+    }
+    
+}
