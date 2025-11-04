2dbccf424159087c77147fe643074815e3aa5663
nuralim25-gif <alimnuralim0411@gmail.com>
2025-11-05 07:25:03 +0800

    Implement odd and even number display program

---
+++ Day59.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Day59 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Input nilai N: ");
+        int n = input.nextInt();
+        
+        String ganjil = "";
+        String genap = "";
+        
+        for (int i = 1; i<=n; i++){
+            if (i%2==1){
+                ganjil += i + " ";
+            } else {
+                genap += i + " ";
+            }
+        }
+        
+        //Menampilkan angka ganjil dari 1-N
+        System.out.printf("%nAngka ganjil dari 1-N(%d): %n%s", n, ganjil);
+        //Menampilkan angka genap dari 1-N
+        System.out.printf("%n%nAngka genap dari 1-N(%d): %n%s", n, genap);
+    }
+    
+}
